8724ade0053b49e9db2ba60d1f66a72f45870ae6
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>
2023-07-21 21:32:47 +0200

    refactor: use BPF_PERF_OUTPUT

--- 08_sync_perf_output.py
+++ 08_sync_perf_output.py
@@ -9,18 +9,29 @@ from bcc.utils import printb
 b = BPF(text="""
 #include <uapi/linux/ptrace.h>
 
+// define output data structure in C
+struct data_t {
+    u32 pid;
+    u64 ts;
+    u64 delta;
+};
+
 BPF_HASH(last);
+BPF_PERF_OUTPUT(events);
 
 int do_trace(struct pt_regs *ctx) {
     u64 ts, *tsp, delta, key = 0;
+    struct data_t data = {};
 
     // attempt to read stored timestamp
     tsp = last.lookup(&key);
     if (tsp != NULL) {
         delta = bpf_ktime_get_ns() - *tsp;
         if (delta < 1000000000) {
-            // output if time is less than 1 second
-            bpf_trace_printk("%d\\n", delta / 1000000);
+            data.pid = bpf_get_current_pid_tgid();
+            data.ts = bpf_ktime_get_ns();
+            data.delta = delta / 1000000;
+            events.perf_submit(ctx, &data, sizeof(data));
         }
         last.delete(&key);
     }
@@ -28,6 +39,7 @@ int do_trace(struct pt_regs *ctx) {
     // update stored timestamp
     ts = bpf_ktime_get_ns();
     last.update(&key, &ts);
+
     return 0;
 }
 """)
@@ -36,13 +48,20 @@ b.attach_kprobe(event=b.get_syscall_fnname("sync"), fn_name="do_trace")
 print("Tracing for quick sync's... Ctrl-C to end")
 
 # format output
+# process event
 start = 0
+def print_event(cpu, data, size):
+    global start
+    event = b["events"].event(data)
+    if start == 0:
+        start = event.ts
+    time_s = (float(event.ts - start)) / 1000000000
+    printb(b"[PID:%6d] At time %.2f ms: multiple syncs detected, last %.2f ms ago" % (event.pid, time_s, event.delta))
+
+# loop with callback to print_event
+b["events"].open_perf_buffer(print_event)
 while 1:
     try:
-        (task, pid, cpu, flags, ts, ms) = b.trace_fields()
-        if start == 0:
-            start = ts
-        ts = ts - start
-        printb(b"At time %.2f s: multiple syncs detected, last %s ms ago" % (ts, ms))
+        b.perf_buffer_poll()
     except KeyboardInterrupt:
         exit()
